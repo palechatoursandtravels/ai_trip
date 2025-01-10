dbcfc921093905f21bace82959c661b8f72132d6
Lokesh <benzendizoniumchloride@gmail.com>
2025-01-10 21:10:14 +0530

    contact expert button implementation modified

---
+++ lib/db/migrations/0006_violet_lorna_dane.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "User" DROP COLUMN IF EXISTS "firstName";--> statement-breakpoint
+ALTER TABLE "User" DROP COLUMN IF EXISTS "lastName";--> statement-breakpoint
+ALTER TABLE "User" DROP COLUMN IF EXISTS "phoneNumber";--> statement-breakpoint
+ALTER TABLE "User" DROP COLUMN IF EXISTS "image";--> statement-breakpoint
+ALTER TABLE "User" DROP COLUMN IF EXISTS "provider";
